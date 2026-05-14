7206b4790c584553a270fd270439972c4f478a35
daniprieto <dani101val@gmail.com>
2026-05-14 19:12:47 +0200

    firsts experiments completed

---
+++ utils.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.manifold import TSNE
+
+
+def visualize_tsne_embeddings(words, embeddings, word_index, title="t-SNE Embeddings", filename=None):
+    """
+    Visualizes t-SNE embeddings of selected words.
+
+    Args:
+        words (list): List of words to visualize.
+        embeddings (numpy.ndarray): Array containing word embeddings.
+        word_index (dict): Mapping of words to their indices in the embeddings array.
+        title (str): Title for the plot.
+        filename (str, optional): File to save the visualization. If None, plot is displayed.
+
+    Returns:
+        None
+    """
+    # Filter the embeddings for the selected words
+    indices = [word_index[word] for word in words]
+    selected_embeddings = embeddings[indices]
+
+    # Set perplexity for t-SNE, it's recommended to use a value less than the number of selected words
+    perplexity = min(5, len(words) - 1)
+
+    # Use t-SNE to reduce dimensionality
+    tsne = TSNE(n_components=2, perplexity=perplexity, random_state=0)
+    reduced_embeddings = tsne.fit_transform(selected_embeddings)
+
+    # Plotting
+    plt.figure(figsize=(10, 10))
+    plt.title(title)
+    for i, word in enumerate(words):
+        plt.scatter(reduced_embeddings[i, 0], reduced_embeddings[i, 1])
+        plt.annotate(
+            word,
+            xy=(reduced_embeddings[i, 0], reduced_embeddings[i, 1]),
+            xytext=(5, 2),
+            textcoords="offset points",
+            ha="right",
+            va="bottom",
+        )
+
+    # Save or display the plot
+    if filename:
+        plt.savefig(filename)
+    else:
+        plt.show()
+
+
+def visualize_all_tsne_embeddings(
+    embeddings, word_index, words_to_plot, words_to_label=None, filename=None
+):
+    """
+    Visualizes t-SNE embeddings of selected words with optional labeling.
+
+    Args:
+        embeddings (numpy.ndarray): Array containing word embeddings.
+        word_index (dict): Mapping of words to their indices in the embeddings array.
+        words_to_plot (list): List of words to plot.
+        words_to_label (list, optional): List of words to label. Defaults to None.
+        filename (str, optional): File to save the visualization. If None, plot is displayed.
+
+    Returns:
+        None
+    """
+    # Create a reverse mapping from index to word
+    index_word = {index: word for word, index in word_index.items()}
+
+    # Ensure words_to_label is a subset of words_to_plot
+    if words_to_label is None:
+        words_to_label = words_to_plot
+    words_to_label = set(words_to_label).intersection(words_to_plot)
+
+    # Filter the embeddings for the words to plot
+    indices_to_plot = [word_index[word] for word in words_to_plot if word in word_index]
+    selected_embeddings = embeddings[indices_to_plot]
+
+    # Set perplexity for t-SNE, it's recommended to use a value less than the number of selected words
+    perplexity = min(5, len(words_to_plot) - 1)
+
+    # Use t-SNE to reduce dimensionality
+    tsne = TSNE(n_components=2, perplexity=perplexity, random_state=0)
+    reduced_embeddings = tsne.fit_transform(selected_embeddings)
+
+    # Plotting
+    plt.figure(figsize=(12, 12))
+    for i, index in enumerate(indices_to_plot):
+        plt.scatter(reduced_embeddings[i, 0], reduced_embeddings[i, 1], alpha=0.5)
+        if index_word[index] in words_to_label:  # Annotate only selected words
+            plt.annotate(
+                index_word[index],
+                xy=(reduced_embeddings[i, 0], reduced_embeddings[i, 1]),
+                xytext=(5, 2),
+                textcoords="offset points",
+                ha="right",
+                va="bottom",
+            )
